2129cc2bed05c303f2b4fdbb99ecdffd56bd27b1
Aditya Kode <aditya.kode21@pccoepune.org>
2024-01-30 01:48:26 +0530

    added code

---
+++ public/code.js
@@ -0,0 +1,180 @@
+// /* cSpell:disable */
+// (function(){
+//     const app = document.querySelector(".app")
+//     const socket = io();
+
+//     let uname;
+//     app.querySelector(".join-screen #join-user").addEventListener("click",function(){
+//         let username = app.querySelector(".join-screen #username").value;
+//         if(username.length == 0){
+//             return;
+//         }
+//         socket.emit("newuser",username);
+//         uname = username;
+//         app.querySelector(".join-screen").classList.remove("active");
+//         app.querySelector(".chat-screen").classList.add("active");
+
+//     });
+
+//     app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
+//         let message = app.querySelector(".chat-screen #message-input").value;
+//         if(message.length == 0){
+//             return;
+//         }
+//         renderMessage("my",{
+//             username:uname,
+//             text:message
+//         });
+//         socket.emit("chat",{
+//             username:uname,
+//             text:message
+//         });
+//         app.querySelector(".chat-screen #message-input").value = "";    
+//     })
+
+//     app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+//         socket.emit("exituser",uname);
+//         window.location.href = window.location.href;
+//     });
+
+//     socket.on("update",function(update){
+//         renderMessage("update",update)
+//     })
+
+//     socket.on("chat",function(message){
+//         renderMessage("other",message)
+//     })
+
+//     function renderMessage(type,message){
+//         let messageContainer =  app.querySelector(".chat-screen .messages");
+//         if(type == "my"){
+//             let el = document.createElement("div");
+//             el.setAttribute("class","message my-message");
+//             el.innerHTML  = `
+//                 <div>
+//                     <div class="name">You</div>
+//                     <div class="text">${message.text}</div>
+//                 </div>
+//             `;
+//             messageContainer.appendChild(el)
+
+//         }else if(type == "other"){
+
+//             let el = document.createElement("div");
+//             el.setAttribute("class","message other-message");
+//             el.innerHTML  = `
+//                 <div>
+//                     <div class="name">${message.username}</div>
+//                     <div class="text">${message.text}</div>
+//                 </div>
+//             `;
+//             messageContainer.appendChild(el)
+
+//         }else if (type == "update"){
+//             let el = document.createElement("div");
+//             el.setAttribute("class","update");
+//             el.innerText  = message;
+//             messageContainer.appendChild(el)
+
+//         }
+//         //scroll chat to end
+//         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+//     }
+
+
+// })();
+
+
+
+
+
+ /// modified code with 'enter' keydown
+
+
+/* cSpell:disable */
+(function () {
+    const app = document.querySelector(".app");
+    const socket = io();
+
+    let uname;
+
+    function joinChat() {
+        let username = app.querySelector(".join-screen #username").value;
+        if (username.length == 0) {
+            return;
+        }
+        uname = username;
+        app.querySelector(".join-screen").classList.remove("active");
+        app.querySelector(".chat-screen").classList.add("active");
+        socket.emit("newuser", uname);
+        // Broadcast to all clients that a new user has joined
+        socket.emit("update", uname + " joined the conversation");
+    }
+
+    app.querySelector(".join-screen #join-user").addEventListener("click", joinChat);
+
+    app.querySelector(".join-screen #username").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            joinChat();
+        }
+    });
+
+    app.querySelector(".chat-screen #message-input").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    });
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click", sendMessage);
+
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
+        socket.emit("exituser", uname);
+        window.location.href = window.location.href;
+    });
+
+    function sendMessage() {
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if (message.length == 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        });
+        socket.emit("chat", {
+            username: uname,
+            text: message
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+    }
+
+    function renderMessage(type, message) {
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if (type === "my" || type === "other") {
+            let el = document.createElement("div");
+            el.setAttribute("class", `message ${type}-message`);
+            el.innerHTML = `
+                <div>
+                    <div class="name">${type === "my" ? "You" : message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        } else if (type === "update") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+
+    socket.on("update", function (update) {
+        renderMessage("update", update);
+    });
+
+    socket.on("chat", function (message) {
+        renderMessage("other", message);
+    });
+
+})();
